bf2fe94cff5aca28fa692d7d1675815eb7ac58f3
liangtongjonker <1018958@hr.nl>
2021-11-16 12:43:03 +0100

    gives location in console (not visible) at incheck

--- CycleCompanion/Inchecken.xaml.cs
+++ CycleCompanion/Inchecken.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace CycleCompanion
 {
@@ -26,6 +27,26 @@ namespace CycleCompanion
         public async void Navigate_Main(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new MainPage());
+        }
+
+        public async void Get_Location(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}\n" +
+                                      $"Longitude: {location.Longitude}\n" +
+                                      $"Altitude: {location.Altitude}\n" +
+                                      $"Speed: {location.Speed}\n");
+                }
+            } catch (FeatureNotSupportedException fnsEx) { Console.WriteLine("Feature not supported."); }
+              catch(FeatureNotEnabledException fneEx) { Console.WriteLine("Feature not enabled."); }
+              catch(PermissionException pEx) { Console.WriteLine("Please enable permission to get location."); }
+              catch(Exception ex) { Console.WriteLine("Unable to get location"); }
+
+            Console.WriteLine("My location");
         }
     }
 }
