687f6c444ab1c72b5fc481370b8d30277896b3f9
liangtongjonker <1018958@hr.nl>
2021-11-17 10:42:34 +0100

    Verbinding database. Werkt nog niet

---
+++ CycleCompanion/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySqlConnector;
+
+namespace CycleCompanion
+{
+    public class Configuration
+    {
+        public static string server = "185.182.56.245";
+        public static string database = "nisatsv379_DeWielrenners";
+        public static string uid = "nisatsv379_DeWielrenners";
+        public static string password = "W7CRx5zGZ";
+
+        public Configuration()
+        {
+        }
+
+        public static string getConnectionString()
+        {
+            var stringBuilder = new MySqlConnectionStringBuilder
+            {
+                Server = server,
+                UserID = uid,
+                Password=password,
+                Database=database,
+            };
+
+
+            return stringBuilder.ConnectionString;
+        }
+    }
+}
