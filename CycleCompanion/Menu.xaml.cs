ba047edb903f6c4c3c8e510284bd01a59830b174
nisatutucu-hr <0999289@hr.nl>
2021-11-18 11:40:50 +0100

    menu aangepast

---
+++ CycleCompanion/Menu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CycleCompanion
+{
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            BindingContext = this;
+            InitializeComponent();
+            
+        }
+        public async void Navigate_Inchecken(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Inchecken());
+        }
+        public async void Navigate_Statistieken(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Statistieken());
+        }
+        public async void Navigate_Profiel(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Profiel());
+        }
+        private async void Navigate_Login(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Login());
+        }
+        public string userName
+        {
+            get
+            {
+                return "Kees";
+            }
+        }
+    }
+}
