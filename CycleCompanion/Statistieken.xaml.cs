5f6c98e9437f2e7a174ed7095a2423c44c30fb75
liangtongjonker <1018958@hr.nl>
2021-11-30 15:26:50 +0100

    Hij laat nu je fietstijd zien

--- CycleCompanion/Statistieken.xaml.cs
+++ CycleCompanion/Statistieken.xaml.cs
@@ -1,22 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using MySqlConnector;
 using Xamarin.Forms;
 
 namespace CycleCompanion
 {
     public partial class Statistieken : ContentPage
     {
+        public static DateTime begintijd { get; set; }
+        public static DateTime eindtijd { get; set; }
+        public static int gemsnelheid { get; set; }
+        public static int maxsnelheid { get; set; }
+        public static double afstand { get; set; }
+
+        public string sbegintijd
+        {
+            get { if (begintijd != default(DateTime)) { return begintijd.ToString("HH:mm"); } else { return "..."; } }
+        }
+
+        public string seindtijd
+        {
+            get { if (eindtijd != default(DateTime)) { return eindtijd.ToString("HH:mm"); } else { return "..."; } }
+        }
+
+        public string fietstijd
+        {
+            get {
+                if (begintijd != default(DateTime) && eindtijd == default(DateTime))
+                { return DateTime.Now.Subtract(begintijd).ToString("hh\\:mm\\:ss") ; }
+                else if (begintijd != default(DateTime) && eindtijd != default(DateTime))
+                {
+                    return eindtijd.Subtract(begintijd).ToString("hh\\:mm\\:ss");
+                }
+                else { return "..."; }
+            }
+        }
+
         public string nummer
         {
             get
             {
-                return "1018958";
+                return "" + Profiel.nummer;
             }
         }
 
         public Statistieken()
         {
+            BindingContext = this;
             InitializeComponent();
         }
 
