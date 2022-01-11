240b43c847b7fd4ac0521d6914877a7c57c5065f
liangtongjonker <1018958@hr.nl>
2022-01-11 15:07:12 +0100

    Nu met lege geschiedenis pagina

---
+++ CycleCompanion/Geschiedenis.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CycleCompanion
+{
+    public partial class Geschiedenis : ContentPage
+    {
+        public Geschiedenis()
+        {
+            InitializeComponent();
+        }
+        public async void Navigate_Menu(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Menu());
+        }
+        public async void Navigate_Profiel(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Profiel());
+        }
+        public async void Navigate_Badge(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Badge());
+        }
+    }
+}
+
